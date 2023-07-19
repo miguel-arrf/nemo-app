5137e1fd4ad33aadf3b0735dcca71ed0bdb3f785
Miguel Ferreira <mferreira.oeiras@gmail.com>
2023-07-19 17:27:21 +0100

    Initial Commit

---
+++ nemo-app/nemo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nemo_appApp.swift
+//  nemo-app
+//
+//  Created by Miguel Ferreira on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct nemo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
